79cd928a1f5174c694bcc47b592d37d992709ed0
hadihakim <hah092@usal.edu.lb>
2022-03-11 20:56:03 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,74 @@
+let categories = ["category1","category2","category3"];
+let selectedCategories = [...categories];
+
+
+const constructUrl = (filters = []) =>{
+  let urlParams = "&category=[";
+  filters.forEach((filter,idx) =>{
+    urlParams += '"'+filter+'"';
+    if(idx < filters.length - 1){
+      urlParams+= ",";
+    }
+
+  })
+  urlParams+= "]";
+  return `https://filltext.com/?rows=10&fname={firstName}&lname={lastName}${urlParams}&pretty=true`
+}
+const loadData = (url) =>{
+  fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(item =>{
+      let cardsContainer = document.getElementById("cardsContainer");
+      let card = createElement("div","",["card"]);
+      let nameContainer = createElement("div",createInitials(item),["nameContainer"]);
+      let usernameContainer = createElement("div",getUserName(item),["usernameContainer"]);
+      let categoryContainer = createElement("div",item.category,["categoryContainer"]);
+      card.append(nameContainer)
+      card.append(usernameContainer)
+      card.append(categoryContainer)
+      console.log(card);
+      cardsContainer.prepend(card);
+    })
+  });
+}
+
+loadData(constructUrl(selectedCategories));
+
+const onCategoryClicked = (name) =>{
+  let index = selectedCategories.findIndex(item => item === name);
+  if(index >= 0){
+    selectedCategories.splice(index,1);
+  }
+  else{
+    selectedCategories.push(name);
+  }
+  console.log(selectedCategories);
+}
+
+
+
+
+
+const createElement = (type, innerHTML, classes, id) =>{
+  let e = document.createElement(type);
+  if(innerHTML) e.innerHTML = innerHTML;
+  if(classes && classes.length){
+    classes.forEach(function(c){
+      e.classList.add(c);
+    })
+  };
+  if(id) e.setAttribute("id",id);
+  return e;
+}
+
+
+
+const createInitials = (data) =>{
+  let output = data.fname.substring(0,1) + data.lname.substring(0,1);
+  return output
+}
+
+const getUserName = (data) =>{
+  return data.fname + data.lname;
+}
